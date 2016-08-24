0dc090150ade82c5b721d3af52c86f117deaff70
Anlibraly <anlibraly@gmail.com>
2016-08-25 02:23:41 +0800

    add login

--- app/lib/common.js
+++ app/lib/common.js
@@ -1,25 +1,29 @@
 function getRootPath(){
     var curWwwPath = window.document.location.href;
-    var pathName = '/'+window.document.location.pathname;
+    var pathName = window.document.location.pathname;
     var pos = curWwwPath.indexOf(pathName);
-    if(pos > 0){
+    if(pos > 8){
     	return curWwwPath.substring(0,pos);
 	}
     return curWwwPath;
 }
 var _dirname = getRootPath();
 
+if(_dirname.lastIndexOf("/") == _dirname.length -1){
+	_dirname = _dirname.substring(0, _dirname.length -1);
+}
+
 $.ajax({
     type: 'get',
-    url: `${_dirname}/system/getsession`,
+    url: _dirname + '/system/getsession',
     dataType: "json",
     success: function(data){
-    	if(data&&data.type === 1){
-    		window.location.href = `${_dirname}/salesman/index.html`;
-    	}else if(data&&data.type === 2){
-    		window.location.href = `${_dirname}/admin/index.html`;
+    	if(data&&data.result.type === 1){
+    		window.location.href = _dirname + '/salesman/index.html';
+    	}else if(data&&data.result.type === 2){
+    		window.location.href = _dirname + '/admin/index.html';
     	}else{
-    		window.location.href = `${_dirname}`;
+    		window.location.href = _dirname;
     	}
     }
 });
