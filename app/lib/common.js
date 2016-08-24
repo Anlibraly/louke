bebcd1ac59de15ec7847135214e95770b3ffe3a2
Anlibraly <anlibraly@gmail.com>
2016-08-25 02:29:21 +0800

    add login moudle

--- app/lib/common.js
+++ app/lib/common.js
@@ -8,15 +8,17 @@ function getRootPath(){
     return curWwwPath;
 }
 var _dirname = getRootPath();
-
+var user_type = 0;
 $.ajax({
     type: 'get',
     url: `${_dirname}/system/getsession`,
     dataType: "json",
     success: function(data){
     	if(data&&data.type === 1){
+    		user_type = 1;
     		window.location.href = `${_dirname}/salesman/index.html`;
     	}else if(data&&data.type === 2){
+    		user_type = 2;
     		window.location.href = `${_dirname}/admin/index.html`;
     	}else{
     		window.location.href = `${_dirname}`;
