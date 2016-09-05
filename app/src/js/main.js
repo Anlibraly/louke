2698346f865a42f6340f3bf8e41712e6cda489df
Anlibraly <anlibraly@gmail.com>
2016-09-05 15:41:52 +0800

    improve code

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
 });
 
 function bdsize(){
-	var bdWidth=$(window).width() -62;
+	var bdWidth=$(window).width();
 	var bdHeight=$(window).height()  ;
 	var mainHeight=$(window).height() - 62;
-	$("#wrapper").css({"width":bdWidth, "height": mainHeight});
-	$("#main_wrap").css({"width":bdWidth, "height": mainHeight});
+	$("#wrapper").css({"width":bdWidth*0.98, "height": mainHeight});
+	$("#main_wrap").css({"width":bdWidth*0.98, "height": mainHeight});
 	$(".menu_bar").css({"bottom": 0});
 }
 
